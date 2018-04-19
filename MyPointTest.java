e3497c3f60e2df64ab8e5f26a3d04c463152abd3
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-19 22:27:19 +0700

    pf-java-oop my point

--- MyPointTest.java
+++ MyPointTest.java
@@ -1,7 +1,7 @@
 public class MyPointTest {
     public static void main(String[] args) {
         MyPoint myPoint = new MyPoint(0,0);
-        MyPoint myPoint1 = new MyPoint(2, 2);
+        MyPoint myPoint1 = new MyPoint(10, 30.5);
         double len = MyPoint.distance(myPoint1,myPoint);
         System.out.println(len);
     }
